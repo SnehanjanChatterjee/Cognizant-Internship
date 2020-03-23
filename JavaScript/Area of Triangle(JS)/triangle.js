527559524919e585f8996289a1e5e5c2f3bd22b8
Snehanjan Chatterjee <41839617+SnehanjanChatterjee@users.noreply.github.com>
2020-03-23 12:32:45 +0530

    Add files via upload

--- JavaScript/Area of Triangle(JS)/triangle.js
+++ JavaScript/Area of Triangle(JS)/triangle.js
@@ -3,7 +3,7 @@ function calarea() {
     var y = document.getElementById("side2").value;
     var z = document.getElementById("side3").value;
     var s = (parseInt(x) + parseInt(y) + parseInt(z)) / 2;
-    var area = Math.sqrt(s * ((s - x) * (s - y) * (s - z)));
+    var area = Math.sqrt(s * ((s - parseInt(x)) * (s - parseInt(y)) * (s - parseInt(z))));
     document.getElementById("area").innerHTML = "Area of triangle is " + area;
     console.log(area);
 }
